9136d1ae143522d3277d213ddf2328ddc92d488c
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-13 09:07:17 -0400

    add html5 declaration

--- application.rb
+++ application.rb
@@ -3,6 +3,8 @@ Bundler.require(:default, (ENV['RACK_ENV'] || :development).to_sym)
 
 module My
   class Application < Sinatra::Base
+    set :haml, format: :html5
+
     get '/' do
       haml :index
     end
