b641fb7ff9c1a106a3cd75b4bae500bfcb47eb4b
Derek Willis <dwillis@gmail.com>
2013-05-07 20:20:17 -0400

    have long urls look for short urls as well

--- times_dialect.rb
+++ times_dialect.rb
@@ -5,6 +5,7 @@ require 'uri'
 require 'rubygems'
 require 'twitter'
 require 'times_wire'
+require 'bitly'
 include TimesWire
 Bundler.require(:default, (ENV['RACK_ENV'] || :development).to_sym)
 
@@ -14,6 +15,7 @@ module TimesDialect
     configure do
       @@config = YAML.load_file("config.yml") rescue nil || {}
       TimesWire::Base.api_key = ENV['TIMESWIRE_API_KEY'] || @@config['times_wire_api_key']
+      Bitly.use_api_version_3
     end
     
     get '/' do
@@ -33,6 +35,7 @@ module TimesDialect
           :oauth_token => ENV['OAUTH_TOKEN'] || @@config['oauth_token'],
           :oauth_token_secret => ENV['OAUTH_TOKEN_SECRET'] || @@config['oauth_token_secret']
         )
+      @bitly = Bitly.new(ENV['BITLY_USER'], ENV['BITLY_API_KEY'])
       if @url.include?('nyti.ms')
         @tw_url = Net::HTTP.get_response(URI.parse(@url))['location'].split('?').first
         @item = Item.url(@tw_url)
@@ -42,8 +45,13 @@ module TimesDialect
           @tweets << tweet unless @tweets.detect{|t| t.id == tweet.id}
         end
       else
+        short_url = @bitly.shorten(@url).short_url
         @item = Item.url(@url)
         @tweets = @client.search(@url.split('?').first).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        tweets2 = @client.search(short_url.split('?').first).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        tweets2.each do |tweet|
+          @tweets << tweet unless @tweets.detect{|t| t.id == tweet.id}
+        end
       end
       erb :result
     end
