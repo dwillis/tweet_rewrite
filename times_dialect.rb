9005e87911b6451437afbb117987cd7ceea717c8
Derek Willis <dwillis@gmail.com>
2014-03-14 15:36:01 -0400

    reduce title tokens to 2 and handle no result_type checked

--- times_dialect.rb
+++ times_dialect.rb
@@ -24,12 +24,12 @@ module TimesDialect
     
     post '/result' do
       @url = params[:url]
-      @result_type = params[:result_type].downcase
+      @result_type = params[:result_type] ? params[:result_type] : 'mixed'
       redirect "/show?url=#{@url}&result_type=#{@result_type}"
     end
 
     get '/show' do
-      @result_type = params[:result_type] ? params[:result_type] : 'mixed'
+      @result_type = params[:result_type].downcase
       @url = params[:url].split('?').first
       @client = Twitter::Client.new(
           :consumer_key => ENV['CONSUMER_KEY'] || @@config['consumer_key'],
@@ -41,16 +41,16 @@ module TimesDialect
       if @url.include?('nyti.ms')
         @tw_url = Net::HTTP.get_response(URI.parse(@url))['location'].split('?').first
         @item = Item.url(@tw_url)
-        @tweets = @client.search(@url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
-        tweets2 = @client.search(@tw_url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        @tweets = @client.search(@url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(2).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        tweets2 = @client.search(@tw_url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(2).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
         tweets2.each do |tweet|
           @tweets << tweet unless @tweets.detect{|t| t.id == tweet.id}
         end
       else
         short_url = @bitly.shorten(@url).short_url
         @item = Item.url(@url)
-        @tweets = @client.search(@url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
-        tweets2 = @client.search(short_url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(5).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        @tweets = @client.search(@url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(2).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
+        tweets2 = @client.search(short_url.split('?').first, :result_type => @result_type).statuses.reject{|i| i.text.include?(@item.title.split.first(2).join(' '))}.reject{|i| i.text[0..1] == 'RT'}
         tweets2.each do |tweet|
           @tweets << tweet unless @tweets.detect{|t| t.id == tweet.id}
         end
